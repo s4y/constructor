66120f586530ba37dd1380d7b86836c66296f318
Sidney San Martín <sidney@s4y.us>
2021-10-05 03:17:26 -0400

    Let framebuffers be transparent

--- static/lib/Framebuffer.js
+++ static/lib/Framebuffer.js
@@ -42,7 +42,7 @@ export default class Framebuffer {
       this.alloc();
     }
     gl.bindFramebuffer(gl.FRAMEBUFFER, this.fb);
-    gl.clearColor(0, 0, 0, 1);
+    gl.clearColor(0, 0, 0, 0);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.viewport(...this.viewport);
     f();
