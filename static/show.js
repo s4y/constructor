d22762dd3b2fe0c79e989c219ae7fcdd1945d042
Sidney San Martín <sidney@s4y.us>
2022-05-02 15:48:09 -0400

    Commit my showfile before changing it

--- static/show.js
+++ static/show.js
@@ -1,102 +1,304 @@
 return {
+
   default: [
     "/shaders/util/header.js",
+    // "/shaders/util/editor.js",
+    // "/shaders/util/editor.s4r",
+    // "/shaders/s4y/nye.js",
+    // "/shaders/s4y/blue.s4r",
+    // "/shaders/s4y/green.s4r",
     // "/shaders/util/black.frag",
-    "/shaders/util/align2.s4r",
-    // "/shaders/util/black.frag",
-    // "/shaders/s4y/train_home.s4r",
-    // "/shaders/util/align.s4r",
-    // "/shaders/s4y/sf.s4r",
-    // "/shaders/s4y/particle.s4r",
-    // "/shaders/s4y/itunes.s4r",
-    // "/shaders/util/video.s4r",
-    // "/shaders/s4y/just_sound.s4r",
-    // "/shaders/s4y/ocean.frag",
-    // "/shaders/s4y/01.frag",
-    // "/shaders/s4y/pulsediamond.s4r",
-    // "/shaders/s4y/cam.s4r",
-    // "/shaders/s4y/cubetunel.frag",
-    // "/shaders/s4y/testpilot.frag",
-    // "/shaders/s4y/wonderbg.s4r",
-    // "/shaders/s4y/relax.s4r",
-    // "/shaders/s4y/just_sound.s4r",
-    // "/shaders/s4y/morningfternoon.s4r",
-    // "/shaders/s4y/planet.frag",
-    // "/shaders/s4y/testpilot.frag",
-    // "/shaders/s4y-wonderville/wondercirc.s4r",
-    // "/shaders/s4y/sup.s4r",
-    // "/shaders/s4y/morningfternoon.s4r",
-    // "/shaders/s4y/flower.s4r",
-    // "/shaders/s4y/rezz.s4r",
-    // "/shaders/hello/hello.frag",
-    // "/shaders/deadmau5/mau53d.frag",
-    // "/shaders/s4y/spkrs.s4r",
     // "/shaders/util/grid.s4r",
-    // "/shaders/t420babe/juxtapose.frag",
-    // "/shaders/t420babe/itchy.frag",
 
-    // "/shaders/t420s4y/exactly-what-i-wanted.s4r",
+    // "/shaders/s4y/blobs.s4r",
+    // "/shaders/s4y/ram.s4r",
 
-    // "/shaders/s4y/2dsea.s4r",
-
-    // "/shaders/s4y/adieu_but_actually_starting_point_for_broken_ray_marching.s4r",
-
-
-    // "/shaders/s4y/cubez3d.frag",
-    // "/shaders/s4y/wonderflow.s4r",
-    // "/shaders/s4y/hazard.s4r",
-    // "/shaders/s4y/90splanet.s4r",
-    // "/shaders/s4y/state.s4r",
-    // "/shaders/s4y/espresso_lab.s4r",
-    // "/shaders/s4y/sqrz.s4r",
     // "/shaders/s4y/morning.s4r",
-    // "/shaders/s4y/edgefade.s4r",
+
+
+
+
+    // "/shaders/util/align2.s4r",
+    // "shaders/s4y/whisps.s4r",
+    // "shaders/s4y/whisps.s4r",
+    // "shaders/s4y/edges.frag",
+
+    // "/shaders/s4y/seal.frag",
+    // "/shaders/s4y/sffancy.s4r",
+    // "/shaders/s4y/streamulate.s4r",
+    // "/shaders/s4y/streamulate.s4r",
+
+    // "/shaders/s4y/surfice.frag",
+
+    // "/shaders/s4y/fademiddleround.s4r",
+
+
+    // "/shaders/s4y/no_crypto.s4r",
+    // "/shaders/s4y/key.s4r",
+
+
+    // "/shaders/s4y/sf.s4r",
+    // "/shaders/s4y/edges.frag",
+    // "/shaders/s4y/rectish.s4r",
+    // "/shaders/s4y/edges.frag",
+
+
+    // "/shaders/s4y/spiral.s4r",
+    // "/shaders/s4y/make_white.s4r",
+    // "/shaders/s4y/edges.frag",
+    // "/shaders/s4y/glass_filt.frag",
+
+
+    // "/shaders/s4y/make_red.s4r",
+    // "/shaders/s4y/make_red.s4r",
+
+
+    // "/shaders/s4y/morning.s4r",
+    // "/shaders/s4y/ocean.frag",
+    // "/shaders/s4y/twitch_ocean.frag",
+    // "/shaders/s4y/make_red.s4r",
+    // "/shaders/s4y/chonkify.s4r",
+
+
+    // "/shaders/s4y/ocean.frag",
+
+    // "/shaders/s4y/rectTun.s4r",
     // "/shaders/s4y/morningfternoon.s4r",
 
-    // "/shaders/s4y/spkrs.s4r",
+    // "/shaders/s4y/sf.s4r",
 
-    // "/shaders/s4y/adieu_but_actually_starting_point_for_broken_ray_marching.s4r",
-    // "/shaders/s4y/kaleid.s4r",
-    //"/shaders/s4y/skin.s4r",
+
+    // "/shaders/s4y/glass_filt.frag",
+
+
+    // "/shaders/s4y/adieu_but_amazingly_broken.s4r",
+    //
+    //
+    //
+    // "/shaders/s4y/glass_filt.frag",
+    // "/shaders/s4y/dusty_bugs.frag",
 
 
     //
-    // "/shaders/s4y/morning.s4r",
-    // "/shaders/s4y-wonderville/dj.s4r",
-    // "/shaders/s4y/light2.s4r",
-    // "/shaders/s4y/video3d.frag",
-    // "/shaders/s4y/90s.s4r",
-    // "/shaders/s4y/dosome.s4r",
-    // "/shaders/shadertoy/proc_ocean.frag",
-    // "/shaders/s4y/glass_ocean.frag",
-    // "/shaders/s4y/ram.s4r",
-    // "/shaders/s4y/horizon.s4r",
-    // "/shaders/s4y/veryedgefade.s4r",
-    // "/shaders/s4y/planet.s4r",
+
+
+
+    // "/shaders/s4y/relax2.s4r",
+    // "/shaders/s4y/relax2.s4r",
+    // "/shaders/s4y/pulsediamond.s4r",
+    // "/shaders/s4y/yet_another_cube.s4r",
+    // "/shaders/s4y/make_red.s4r",
+    // "/shaders/s4y/streamulate.s4r",
+    // "/shaders/s4y/scan_line.s4r",
+
+    // "/shaders/s4y/3d_stars.s4r",
+    // "shaders/s4y/order_3.s4r",
+    // "/shaders/s4y/black_hole.s4r",
+    // { 0.5 *
+    //   path: "/shaders/s4y/order_5b.s4r",
+    //   flash: 0,
+    //   params: {
+    //     brightness(ctx) { if (ctx.midi.button12) this.flash = 1; return this.flash*=0.99; }
+    //   },
+    //   if(ctx) { return this.flash > 0; },
+    // },
+
+
+    // "/shaders/s4y/wonderflow.s4r",
+
+    // "/shaders/s4y/shapes.s4r",
+    // "/shaders/s4y/sqrz.s4r",
+
+
+    // "/shaders/s4y/the_matrix.s4r",
+    // "/shaders/s4y/reel_1.s4r",
+    // "/shaders/s4y/streamulate.s4r",
+    // "/shaders/s4y/redSlice.s4r",
+    // "/shaders/s4y/whisps.s4r",
+    // "/shaders/s4y/chonkify.s4r",
+//    "/shaders/s4y/reel_1.s4r",
+
+
+    // "/shaders/util/angel_grid.s4r",
+    // "/shaders/s4y/train_home.s4r",
+    // "/shaders/s4y/sffancy.s4r",
+    // "/shaders/util/grid_move.s4r",
+
+    // "/shaders/s4y/blobbyjamz.s4r",
+
+    // "/shaders/s4y/lattice.s4r",
+
+    // "/shaders/s4y/make_red.s4r",
+    // "/shaders/s4y/streamulate.s4r",
+    // "/shaders/s4y/redSlice.s4r",
+    // "/shaders/s4y/inv.s4r",
+    // "/shaders/s4y/make_red.s4r",
+    // "/shaders/s4y/bean.s4r",
+    // "/shaders/s4y/streamulate.s4r",
+    // "/shaders/s4y/embrighten.s4r",
+    "/shaders/s4y/morningfternoon.s4r",
+    // "/shaders/s4y/sponge.frag",
+
+    // "/shaders/s4y/rectTun.s4r",
+
+    // {
+    //   path: "/shaders/s4y/make_red.s4r",
+    //   params: {
+    //   },
+    //   if(ctx) { return ctx.midi.button11; },
+    // },
+
+
+
+    // "/shaders/s4y/glass_filt.frag",
+
+
+    // "/shaders/s4y/edges.frag",
+
+    // "/shaders/s4y/edges.frag",
+    "/shaders/s4y/streamulate.s4r",
+    // "/shaders/s4y/chonkify.s4r",
+    // "/shaders/s4y/streamulate.s4r",
+    // "/shaders/s4y/sqrz.s4r",
+    // "/shaders/s4y/ice_ice_bby.frag",
+    // "/shaders/s4y/streamulate.s4r",
+    // "/shaders/s4y/reel_1.s4r",
+    // "/shaders/s4y/streamulate.s4r",
+
+    // "/shaders/s4y/stars.s4r",
+    // "/shaders/s4y/stars.s4r",
+    // "/shaders/s4y/bean.s4r",
+
+    // "/shaders/s4y/ben.s4r",
+
+    //
+    // "/shaders/s4y/streamulate.s4r",
+    // "/shaders/s4y/jellyCube.frag",
+
+
+
+
     // "/shaders/util/editor.s4r",
-    // "/shaders/s4y/cuuuube.s4r",
-    // "/shaders/s4y/fademiddle.s4r",
 
+    // "/shaders/util/editor.s4r",
+
+
+    // "/shaders/s4y/cam.s4r",
+
+    // "/shaders/s4y/cam.s4r",
+
+    // "/shaders/s4y/snapshot.js",
+    // "/shaders/s4y/snapshot.s4r",
+
+    // "/shaders/s4y/boxify.s4r",
+
+
+
+    // "/shaders/s4y/clouds.frag",
+
+    // "/shaders/s4y/bpm.s4r",
+    // "/shaders/s4y/kaleid.s4r",
+    // "/shaders/s4y/streamulate.s4r",
+
+
+
+    // "/shaders/util/beat_clock.s4r",
+    // "/shaders/util/beat_shake.s4r",
+// 
+    // "/shaders/s4y/beat_glitch.s4r",
+    // "/shaders/s4y/videoglitch.s4r",
+
+    // {
+    //   path: "/shaders/s4y/inv.s4r",
+    //   params: {
+    //     _flash: 0,
+    //     _fade: 0,
+    //     _lastBeat: 0,
+    //     flash() {
+    //       this.params._flash *= 0.99;
+    //       const beat = Math.floor(ctx.params.beat / Math.max(1/4, (Math.floor(ctx.midi.knob7 * 4) / 4)));
+    //       if (beat != this.params._lastBeat) {
+    //         this.params._flash = 1;
+    //         this.params._lastBeat = beat;
+    //       }
+    //       return this.params._flash;
+    //     },
+    //     fade() {
+    //       console.log(this.params._fade);
+    //       this.params._fade *= 0.95;
+    //       if (ctx.midi.kbutton7)
+    //         this.params._fade = 1;
+    //       return this.params._fade;
+    //     }
+    //   },
+    //   if(ctx) { return this.params.fade() > 0; },
+    // },
+    // "/shaders/util/build_flash.s4r",
+
+
+    // "/shaders/s4y/reel_1.s4r",
+    //
+
+    // "/shaders/s4y/tangents.s4r",
+    // "/shaders/s4y/bighalo.s4r",
+
+    // "/shaders/util/beat_flash.s4r",
+
+    // "/shaders/s4y/rectTun.s4r",
+    {
+      path: "/shaders/s4y/glitch.s4r",
+      params: {
+        // colorChaos: 1,
+        colorChaos(ctx) { return ctx.midi.knob4; }
+      },
+      if(ctx) { return ctx.midi.button9; },
+    },
+
+    // "/shaders/s4y/angel_mike_3d.s4r",
+    // "/shaders/s4y/ice.frag",
+    // "/shaders/s4y/halo.s4r",
+    // "/shaders/s4y/streamulate.s4r",
+    // "/shaders/s4y/beatTunnel.frag",
+    // "/shaders/s4y/chonkify.s4r",
+    // "/shaders/s4y/chonkify.s4r",
+    // "/shaders/s4y/streamulate.s4r",
+    // "/shaders/s4y/dosome.s4r",
+
+    // "/shaders/s4y/kaleid.s4r",
+
+    {
+      path: "/shaders/util/beat_highlight.s4r",
+      params: {
+        mult: 1,
+      },
+    },
+
+    {
+      path: "/shaders/s4y/inv.s4r",
+      params: {
+        _amt: 0,
+        amt(ctx) { this.params._amt *= 0.95; if (ctx.midi.button10) this.params._amt = 1; return this.params._amt; },
+      },
+      if(ctx) { return this.params.amt.call(this, ctx) > 0; },
+    },
+
+    {
+      path: "/shaders/s4y/stall.s4r",
+      params: {
+        amt: 0,
+      },
+      if(ctx) { const ret = ctx.midi.button11; this.params.amt = (ret > 0) ? 1 : 0; return ret; },
+    },
+
+    {
+      path: "/shaders/s4y/blackout.s4r",
+      params: {
+        brightness(ctx) { return ctx.midi.kbutton12 || ctx.midi.knob12; }
+      },
+      if(ctx) { return this.params.brightness.call(this, ctx) > 0; },
+    },
 
   ],
-  big: [
-    "/shaders/util/black.frag",
-    // "/shaders/s4y-wonderville/wonderect.s4r",
-    // "/shaders/s4y-wonderville/wondercirc.s4r",
-    // "/shaders/s4y/morningfternoon.s4r",
-    // "/shader/util/align.s4r",
-    // "/shaders/s4y-wonderville/light.s4r",
-    // "/shaders/s4y-wonderville/wonderect.s4r",
-    "/shaders/s4y-wonderville/bigfade.s4r",
-    // "/shaders/util/grid.s4r",
-
-
-    // "/shaders/s4y/k1.s4r",
-    // "/shaders/s4y/light.s4r",
-  ],
-  "2": [
-    "/shaders/util/black.frag",
-    "/shaders/util/align2.s4r",
-    // "/shaders/s4y/hazard.s4r",
+  avatar: [
   ],
 };
